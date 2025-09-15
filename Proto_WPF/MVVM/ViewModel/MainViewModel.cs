3e01110c6653a63efe64e80960628617ccf5ed1d
DonnyWu <donny.wu4@gmail.com>
2025-09-15 00:28:47 -0400

    Feature--Added MianViewModel

---
+++ Proto_WPF/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Proto_WPF.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proto_WPF.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
